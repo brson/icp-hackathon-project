85ee73e565ef6f2161dfca3cc8a7dabe83d9109d
Brian Anderson <andersrb@gmail.com>
2022-05-24 17:23:14 -0600

    begin outlining some ui

--- attempt1/src/wiki_frontend/src/components/CanisterIds.svelte
+++ attempt1/src/wiki_frontend/src/components/CanisterIds.svelte
@@ -1,18 +1,13 @@
-<div>
-  <p>Backend canister ID: {process.env.WIKI_BACKEND_CANISTER_ID}</p>
-  <p>Frontend canister ID: {process.env.WIKI_FRONTEND_CANISTER_ID}</p>
-  <p>
-    Internet Identity canister ID: {process.env.INTERNET_IDENTITY_CANISTER_ID}
-  </p>
-</div>
+<section>
+  <p>Page backend canister ID: {PAGE_BACKEND_CANISTER_ID}</p>
+  <p>Wiki backend canister ID: {WIKI_BACKEND_CANISTER_ID}</p>
+  <p>Wiki rontend canister ID: {WIKI_FRONTEND_CANISTER_ID}</p>
+  <p>Internet Identity canister ID: {INTERNET_IDENTITY_CANISTER_ID}</p>
+</section>
 
-<style>
-  p {
-    color: #666;
-    font-size: 14px;
-    margin: 0 0 8px;
-  }
-  div {
-    margin-top: 40px;
-  }
-</style>
+<script>
+  export const PAGE_BACKEND_CANISTER_ID = process.env.PAGE_BACKEND_CANISTER_ID;
+  export const WIKI_BACKEND_CANISTER_ID = process.env.WIKI_BACKEND_CANISTER_ID;
+  export const WIKI_FRONTEND_CANISTER_ID = process.env.WIKI_FRONTEND_CANISTER_ID;
+  export const INTERNET_IDENTITY_CANISTER_ID = process.env.INTERNET_IDENTITY_CANISTER_ID;
+</script>
